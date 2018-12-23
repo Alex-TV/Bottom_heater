88e438ed25b976b9187914b8d22c02fc1e64d755
Alexander Kovalev <kovalev_sasha82@mail.ru>
2018-12-23 22:23:14 +0300

    добавил ядро для Wavgat

---
+++ Code/Bottom_heater/Bottom_heater/ArduinoCoreWavgat/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 04.11.2018 23:10:11
+ * Author : koval
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
